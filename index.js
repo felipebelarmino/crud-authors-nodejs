6cba628a4b0593784d262883c951e22fd6c219e3
felipebelarmino <epilefgomes1@gmail.com>
2021-04-19 20:42:41 -0300

    Prettier format

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-const bodyParser = require('body-parser');
-const express = require('express');
+const bodyParser = require("body-parser");
+const express = require("express");
 const app = express();
 const port = 3000;
-const Author = require('./models/Author');
+const Author = require("./models/Author");
 
 app.use(bodyParser.json());
 
-app.get('/authors', async (req, res) => {
+app.get("/authors", async (req, res) => {
   const authors = await Author.gerAll();
 
   res.status(200).json(authors);
 });
 
-app.get('/authors/:id', async (req, res) => {
+app.get("/authors/:id", async (req, res) => {
   const { id } = req.params;
   const author = await Author.findById(id);
 
-  if(!author) return res.status(404).json({ message: 'Author not found' });
+  if (!author) return res.status(404).json({ message: "Author not found" });
   res.status(200).json(author);
 });
 
-app.post('/authors', async (req, res) => {
+app.post("/authors", async (req, res) => {
   const { first_name, middle_name, last_name } = req.body;
 
   const author = await Author.findByName(first_name, middle_name, last_name);
 
-  console.log(author)
+  console.log(author);
 
-  if(author) return res.status(404).json(`${author.fullName} já existe no banco de dados!`);
+  if (author)
+    return res
+      .status(404)
+      .json(`${author.fullName} já existe no banco de dados!`);
 
   await Author.create(first_name, middle_name, last_name);
 
-  res.status(201).json({ message: 'Author criado com sucesso!'});
+  res.status(201).json({ message: "Author criado com sucesso!" });
 });
 
-app.listen(port, () => console.log('listening on port 3000'));
+app.listen(port, () => console.log("listening on port 3000"));
--- models/connection.js
+++ models/connection.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const connection = mysql.createPool({
-  user: 'root',
-  password: '@D14m09#**651205',
-  host: 'localhost',
-  database: 'model_example',
+  user: "root",
+  password: "@D14m09#**651205",
+  host: "localhost",
+  database: "model_example",
 });
 
 module.exports = connection;
